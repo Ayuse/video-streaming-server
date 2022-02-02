db5c07abd614c315eeace2b86ce1bb8f3ef23727
Ayuse <33384123+Ayuse@users.noreply.github.com>
2022-02-02 17:15:52 -0500

    /video endpoint

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const videoPath = 'video.mp4';
+const videoSize = fs.statSync('video.mp4').size;
+const CHUNK_SIZE = 10 ** 6; //1MB
+const start = Number(range.replace(/\D/g, ''));
+const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/', (req, res) => {
+  const range = req.headers.range;
+  if (!range) {
+    res.status(400).send('Requires Range Header');
+  }
+  const videoPath = 'video.mp4';
+  const videoSize = fs.statSync('video.mp4').size;
 });
 
 app.listen(5000, () => {
